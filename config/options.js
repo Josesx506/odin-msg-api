c776c08a4a95134076562e141c5b03d4a2e51c35
Josesx506 <45031412+Josesx506@users.noreply.github.com>
2025-05-06 08:32:16 -0700

    preflight cors for express 5 pattern

--- config/options.js
+++ config/options.js
@@ -17,9 +17,8 @@ const corsOptions = {
   origin: (origin, callback) => {
     if (!origin) return callback(null, true); // allow Postman, etc.
 
-    const isWhitelisted = allowedOrigins.includes(origin) 
-    // ||
-    //   /^https:\/\/odin-msg-client-.*\.vercel\.app$/.test(origin); // preview links
+    const isWhitelisted = allowedOrigins.includes(origin) ||
+      /^https:\/\/odin-msg-client-.*\.vercel\.app$/.test(origin); // preview links
 
     if (isWhitelisted) {
       callback(null, true);
--- servers/express.js
+++ servers/express.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS middleware
 app.use(credentials);
-app.options('*', cors(corsOptions)); // Preflight cors
+app.options('/{*path}', cors(corsOptions)); // Preflight cors
 // app.use(cors(corsOptions));
 
 // Middleware for auth
