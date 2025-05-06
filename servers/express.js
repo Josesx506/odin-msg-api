69c4d3c7db8424552667aea45bae0b3ac2646bd2
Josesx506 <45031412+Josesx506@users.noreply.github.com>
2025-05-06 07:51:53 -0700

    remove all cors preflightinh

--- servers/express.js
+++ servers/express.js
@@ -19,7 +19,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS middleware
 app.use(credentials);
-app.options('/v1/*name', cors(corsOptions)); // Preflight cors
 app.use(cors(corsOptions));
 
 // Middleware for auth
